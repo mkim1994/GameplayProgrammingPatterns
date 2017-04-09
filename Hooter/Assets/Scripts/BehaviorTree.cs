c35cb1a2b4306cf271a87329733fd36d5e72ba25
mkim1994 <mkim2@andrew.cmu.edu>
2017-04-08 20:25:19 -0400

    behavior tree

---
+++ Hooter/Assets/Scripts/BehaviorTree.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace BehaviorTree{
+
+	//reports success or failure when it's updated. thus, bool.
+	//takes a context variable so that leaf nodes know what to operate on.
+	public abstract class Node<T>{
+		public abstract bool Update(T context);
+	}
+
+	//tree is just a special node that contains the "root." The root is where the execution of the tree starts.
+	//trees are just regular nodes, so they can just be added to other trees as subtrees.
+	public class Tree<T> : Node<T>{
+		private readonly Node<T> _root;
+
+		public Tree(Node<T> root){
+			_root = root;
+		}
+
+		public override bool Update(T context){
+			return _root.Update(context);
+		}
+
+	}
+
+	/* NODE TYPES */
+
+	//action nodes; the leaves. Where the tree reaches a decision of what to do/performs some aspect of the behavior
+	//game-specific actions, so there will most likely be subclasses for these
+	public class Do<T> : Node<T>{
+		public delegate bool NodeAction(T context);
+		private readonly NodeAction _action;
+		public Do(NodeAction action){
+			_action = action;
+		}
+		public override bool Update(T context){
+			return _action (context);
+		}
+	}
+
+	//condition; leaf nodes that test something
+	public class Condition<T> : Node<T>{
+		private readonly Predicate<T> _condition;
+		public Condition(Predicate<T> condition){
+			_condition = condition;
+		}
+		public override bool Update(T context){
+			return _condition (context);
+		}
+	}
+
+	//decision nodes
+	//inner node: has a set of child or branch nodes. these are nodes that define structure/logic
+	//these nodes don't actually "do" anything
+	public abstract class BranchNode<T> : Node<T>{
+		protected Node<T>[] Children { get; private set; }
+		protected BranchNode(params Node<T>[] children){
+			Children = children;
+		}
+	}
+
+	//selector
+	//succeeds when ONE of its children succeeds
+	//fails when ALL of its children fails
+	public class Selector<T> : BranchNode<T>{
+		public Selector(params Node<T>[] children) : base(children) {
+		}
+
+		public override bool Update(T context){
+			foreach (var child in Children) {
+				if (child.Update (context))
+					return true;
+			}
+			return false;
+		}
+	}
+
+	//sequence
+	//succeeds when ALL of its children succeeds
+	//fails when ONE of its children fails
+	//used for things like checklists (e.g. if there's food nearby, and if i'm hungry, then eat the food)
+	public class Sequence<T> : BranchNode<T>{
+		public Sequence(params Node<T>[] children) : base(children){
+		}
+		public override bool Update(T context){
+			foreach (var child in Children) {
+				if (!child.Update (context))
+					return false;
+			}
+			return true;
+		}
+	}
+
+	//decorator
+	//acts as a modifier for another node
+	//base class that holds a reference to the modified or decorated node
+	public abstract class Decorator<T> : Node<T>{
+		protected Node<T> Child { get; private set;}
+		protected Decorator(Node<T> child){
+			Child = child;
+		}
+	}
+
+	//common example of a decorator node: Not or Negate node
+	//inverts the result of another node. 
+	//e.g. if you have a node that checks if there's any friends nearby, you may need to run some logic when there are NO friends nearby.
+	//Not node allows you to do without having to create a different node
+	public class Not<T> : Decorator<T>{
+		public Not(Node<T> child) : base(child){
+		}
+		public override bool Update(T context){
+			return !Child.Update(context);
+		}
+	}
+}
---
+++ Hooter/Assets/Scripts/EnemyE.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using BehaviorTree; //importing namespace
+
+public class EnemyE : Enemy {
+
+	private Tree<EnemyE> _tree;
+	private float angle, radius;
+	public float speed;
+	private float speedfactor;
+
+	private Player player;
+
+
+	// Use this for initialization
+	void Start () {	
+		//define the tree and use a selector at the root to pick the high level behavior
+		_tree = new Tree<EnemyE> (new Selector<EnemyE> (
+			//highest priority
+			//Flee behavior
+			new Sequence<EnemyE> (
+				new IsHitWhilePulsing (),
+				new Flee ()
+			),
+			//fight behavior
+			new Sequence<EnemyE> (
+				new Not<EnemyE>(new IsHitWhilePulsing()),
+				new Not<EnemyE>(new FinishedPreparing()),
+				new Attack ()
+			),
+
+			//prepare to attack
+			new Sequence<EnemyE>(
+				new IsNearPlayer(),
+				new Not<EnemyE>(new IsHitWhilePulsing()),
+				new PrepareToAttack()
+			),
+			//seek behavior
+			new Sequence<EnemyE>(
+				new Not<EnemyE>(new IsNearPlayer()),
+				new Not<EnemyE>(new IsFleeing()),
+				new Seek()
+			)
+		));
+
+		//someListener = new UnityAction ();
+
+		player = GameObject.FindWithTag ("Player").GetComponent<Player> ();
+
+
+
+		type = "E";
+		hp = 2;
+		dmg = 1;
+
+
+
+
+		PlaySound ("enemyDspawn", 1f);
+
+	}
+
+	void Update(){
+		//Move ();
+		_tree.Update(this);
+		DestroyEnemyCheck ("E");
+	}
+
+	public override void Move (){
+		transform.LookAt (player.transform);
+		if (Vector3.Distance (transform.position, player.transform.position) >= 0f) {
+			transform.position += transform.forward * 4f * Time.deltaTime;
+			if (Vector3.Distance (transform.position, player.transform.position) <= 10f) {
+			}
+		}
+	}
+
+	public void MoveStraight(Transform dest){
+		transform.LookAt (dest);
+		if (Vector3.Distance (transform.position, dest.position) >= 0f) {
+			transform.position += transform.forward * 10f * Time.deltaTime;
+		}
+	}
+
+	public void Pulse(){
+		
+		transform.position += new Vector3 (Random.Range (-0.1f, 0.1f), Random.Range (-0.1f, 0.1f), 0f);
+	}
+
+
+
+
+	public override void increaseSpeed(float f){
+	}
+
+
+
+	/* NODES */
+
+	//conditions
+	private class IsNearPlayer : Node<EnemyE>{
+		public override bool Update(EnemyE enemy){
+			return true;
+		}
+
+	}
+
+	private class IsHitWhilePulsing : Node<EnemyE>{
+		public override bool Update(EnemyE enemy){
+			return true;
+		}
+	}
+
+	private class FinishedPreparing : Node<EnemyE>{
+		public override bool Update(EnemyE enemy){
+			return true;
+		}
+	}
+
+	private class IsFleeing : Node<EnemyE>{
+		public override bool Update(EnemyE enemy){
+			return true;
+		}
+	}
+
+	//actions: true means keep going, false means stop; so kind of like a while loop
+
+	private class Flee : Node<EnemyE>{
+		public override bool Update(EnemyE enemy){
+			return true;
+		}
+	}
+
+	private class Seek : Node<EnemyE>{
+		public override bool Update(EnemyE enemy){
+			return true;
+		}
+	}
+
+	private class PrepareToAttack : Node<EnemyE>{
+		public override bool Update(EnemyE enemy){
+			return true;
+		}
+	}
+
+	private class Attack : Node<EnemyE>{
+		public override bool Update(EnemyE enemy){
+			return true;
+		}
+	}
+}
